c969dabd3f9ac5a7d3cb9a57ad82e22ec33bec65
Anastasiya Turenko <oxtnpi@gmail.com>
2019-11-04 21:54:34 +0300

    Добавляет модуль работы с сервером

---
+++ js/errorMessage.js
@@ -0,0 +1,29 @@
+'use strict';
+(function () {
+
+  var errorTemplate = document.querySelector('#error')
+    .content
+    .querySelector('.error');
+  var main = document.querySelector('main');
+  var renderError = function (err) {
+    var errorElement = errorTemplate.cloneNode(true);
+    var errorText = document.querySelector('#error')
+      .content
+      .querySelector('.error__message');
+    errorText.textContent = err;
+
+    main.appendChild(errorElement);
+    var errorConteiner = document.querySelector('.error');
+    var errorButton = document.querySelector('.error__button');
+    errorButton.focus();
+    errorButton.addEventListener('click', function () {
+      main.removeChild(errorConteiner);
+    });
+  };
+
+  window.errorMessage = {
+    show: function (message) {
+      renderError(message);
+    }
+  };
+})();
