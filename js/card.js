98e2211ca351ad6c0642a86d8a1fb066bd7ce8a1
Anastasiya Turenko <oxtnpi@gmail.com>
2019-11-12 22:50:32 +0300

    Fixed adding photos and house type

--- js/card.js
+++ js/card.js
@@ -6,20 +6,43 @@
 
   var filterscontainer = document.querySelector('.map__filters-container');
 
-
   window.card = {
     render: function (data) {
       var cardElement = cardTemplate.cloneNode(true);
       cardElement.querySelector('.popup__title').textContent = data.offer.title;
       cardElement.querySelector('.popup__text--address').textContent = data.offer.address;
       cardElement.querySelector('.popup__text--price').textContent = data.offer.price + '₽/ночь';
-      cardElement.querySelector('.popup__type').textContent = data.offer.type;
+
+      var houseType;
+      switch (data.offer.type) {
+        case 'flat':
+          houseType = 'Квартира';
+          break;
+        case 'bungalo':
+          houseType = 'Бунгало';
+          break;
+        case 'house':
+          houseType = 'Дом';
+          break;
+        case 'palace':
+          houseType = 'Дворец';
+          break;
+      }
+      cardElement.querySelector('.popup__type').textContent = houseType;
       cardElement.querySelector('.popup__text--capacity').textContent = data.offer.rooms + ' комнаты для ' + data.offer.guests;
       cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
       cardElement.querySelector('.popup__features').textContent = data.offer.features;
       cardElement.querySelector('.popup__description').textContent = data.offer.description;
-      cardElement.querySelector('.popup__photos').src = data.offer.photos;
       cardElement.querySelector('.popup__avatar').src = data.author.avatar;
+      var popupPhotos = cardElement.querySelector('.popup__photos');
+      var popupPhoto = cardElement.querySelector('.popup__photo');
+      var photoTemplate = popupPhoto.cloneNode();
+      popupPhotos.removeChild(popupPhoto);
+      data.offer.photos.forEach(function (item) {
+        var newPhoto = photoTemplate.cloneNode();
+        newPhoto.src = item;
+        popupPhotos.appendChild(newPhoto);
+      });
 
       return cardElement;
     },
